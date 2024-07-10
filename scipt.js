e40ba577eae1c2164f21f2ae31e4ad4361fc1203
rfrancodev <francorfs@icloud.com>
2024-07-10 17:40:12 -0300

    Alerta de carrinho

--- scipt.js
+++ scipt.js
@@ -76,7 +76,7 @@ function updateCartModal() {
                 <p class="font-medium mt-2 ">${item.price.toFixed(2)}<p/>
             </div>
 
-            <button>
+            <button class="remove-from-cart-btn" data-name="${item.name}">
                 Remover
             </button>
         
@@ -97,3 +97,48 @@ function updateCartModal() {
 
 }
 
+// REMOVER QUANTIDADE DO CARRINHO
+
+cartItemsContainers.addEventListener('click', function (event) {
+    if (event.target.classList.contains('remove-from-cart-btn')) {
+        const name = event.target.getAttribute('data-name')
+
+        removerItemCart(name)
+    }
+})
+
+function removerItemCart(name) {
+    const index = cart.findIndex(item => item.name === name);
+
+    if (index !== -1) {
+        const item = cart[index];
+
+        if (item.quantity > 1) {
+            item.quantity -= 1;
+            updateCartModal()
+            return
+        }
+
+        cart.splice(index, 1);
+        updateCartModal()
+    }
+}
+
+addressInput.addEventListener('input', function (event) {
+    let inputValue = event.target.value;
+    if (inputValue !== "") {
+        addressInput.classList.remove('border-red-500')
+        addressWarm.classList.add('hidden')
+    }
+
+})
+
+checkoutBtn.addEventListener('click', function () {
+    if (cart.length === 0) return;
+        if (addressInput.value === "") {
+            addressWarm.classList.remove('hidden')
+            addressInput.classList.add('border-red-500')
+            return;
+        }
+    
+})
